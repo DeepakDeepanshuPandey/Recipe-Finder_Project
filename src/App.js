ccf33e903e2ebafe1087806c59998f652e0ae7de
DeepakDeepanshuPandey <84906772+DeepakDeepanshuPandey@users.noreply.github.com>
2021-07-13 23:43:44 +0530

    Commited Code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/App.css";
+import HeaderForm from "./HomePage";
+
+function App() {
+  return (
+    <div>
+      <HeaderForm />
+    </div>
+  );
+}
+
+export default App;
